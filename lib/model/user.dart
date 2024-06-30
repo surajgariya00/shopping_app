dd1a464634a8ae1d9ae1ae7cbba93d5f5984954e
Suraj Singh Gariya <42574041+surajgariya00@users.noreply.github.com>
2024-06-30 23:02:20 +0530

    testing

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,12 +1,4 @@
 class User {
-  final String id;
-  final String name;
-  final String email;
-  final String password;
-  final String address;
-  final String type;
-  final String token;
-
   User({
     required this.id,
     required this.name,
@@ -17,20 +9,6 @@ class User {
     required this.token,
   });
 
-  // Convert a User object to a JSON map.
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'email': email,
-      'password': password,
-      'address': address,
-      'type': type,
-      'token': token,
-    };
-  }
-
-  // Factory constructor for creating a new User instance from a map
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'] as String,
@@ -41,5 +19,25 @@ class User {
       type: json['type'] as String,
       token: json['token'] as String,
     );
+  }
+
+  final String address;
+  final String email;
+  final String id;
+  final String name;
+  final String password;
+  final String token;
+  final String type;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+      'address': address,
+      'type': type,
+      'token': token,
+    };
   }
 }
